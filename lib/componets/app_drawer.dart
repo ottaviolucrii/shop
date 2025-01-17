587a7bdc4d746afc3215e44199334f429e5e0e5d
Ottavio Lucri De Souza <ottaviolucrinot@gmail.com>
2025-01-17 18:17:38 -0300

    Desenvolvi e deixei o app td certo para apresentar

--- lib/componets/app_drawer.dart
+++ lib/componets/app_drawer.dart
@@ -1,36 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/providers/theme_provider.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final iconColor = Theme.of(context).iconTheme.color;
+
     return Drawer(
       child: Column(
         children: [
           AppBar(
             backgroundColor: Theme.of(context).colorScheme.primary,
-            title: const Text('Welcome, User!',
-                style: TextStyle(
-                  color: Colors.white,
-                )),
+            title: const Text(
+              'Welcome, User!',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
             automaticallyImplyLeading: false,
           ),
           const Divider(),
           ListTile(
-            leading: const Icon(Icons.shop),
+            leading: Icon(Icons.shop, color: iconColor),
             title: const Text('Shop'),
             onTap: () {
-              Navigator.of(context).pushReplacementNamed('/');
+              Navigator.of(context).pushReplacementNamed(AppRoutes.HOME);
             },
           ),
           const Divider(),
           ListTile(
-            leading: const Icon(Icons.payment),
+            leading: Icon(Icons.payment, color: iconColor),
             title: const Text('Orders'),
             onTap: () {
-              Navigator.of(context).pushReplacementNamed('/orders');
+              Navigator.of(context).pushReplacementNamed(AppRoutes.ORDERS);
             },
+          ),
+          const Divider(),
+          Consumer<ThemeProvider>(
+            builder: (context, themeProvider, child) => SwitchListTile(
+              inactiveThumbColor: Colors.white,
+              inactiveTrackColor: Colors.red,
+              activeTrackColor: Colors.green,
+              activeColor: Colors.black,
+              title: const Text('Dark Mode'),
+              secondary: Icon(
+                themeProvider.isDarkMode ? Icons.dark_mode : Icons.light_mode,
+                color: iconColor,
+              ),
+              value: themeProvider.isDarkMode,
+              onChanged: (value) {
+                themeProvider.toggleTheme();
+              },
+            ),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/models/cart.dart';
-import 'package:shop/models/order_list.dart';
+import 'package:shop/providers/theme_provider.dart';
 import 'package:shop/screens/cart_screen.dart';
 import 'package:shop/screens/orders_screen.dart';
 import 'package:shop/screens/product_detail_screen.dart';
 import 'package:shop/screens/product_overview_screen.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 
 void main() {
@@ -20,26 +21,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => ProductList(),
-        ),
-        ChangeNotifierProvider(create: (ctx) => Cart()),
-        ChangeNotifierProvider(create: (ctx) => OrderList()),
+        ChangeNotifierProvider(create: (_) => ProductList()),
+        ChangeNotifierProvider(create: (_) => Cart()),
+        ChangeNotifierProvider(create: (_) => OrderList()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
       ],
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple).copyWith(secondary: Colors.white),
-          fontFamily: 'Lato',
-        ),
-        routes: {
-          AppRoutes.HOME : (ctx) => ProductOverviewScreen(),
-          AppRoutes.PRODUCT_DETAIL : (ctx) => ProductDetailScreen(),
-          AppRoutes.CART : (ctx) => CartScreen(),
-          AppRoutes.ORDERS : (ctx) => OrdersScreen(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple).copyWith(secondary: Colors.white),
+              fontFamily: 'Lato',
+              iconTheme: IconThemeData(color: Colors.black), // Ícones pretos no modo claro
+            ),
+            darkTheme: ThemeData.dark().copyWith(
+              colorScheme: ColorScheme.dark().copyWith(secondary: Colors.white),
+              iconTheme: IconThemeData(color: Colors.white), // Ícones brancos no modo escuro
+            ),
+            themeMode: themeProvider.themeMode,
+            routes: {
+              AppRoutes.HOME: (ctx) => ProductOverviewScreen(),
+              AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+              AppRoutes.CART: (ctx) => CartScreen(),
+              AppRoutes.ORDERS: (ctx) => OrdersScreen(),
+            },
+            debugShowCheckedModeBanner: false,
+          );
         },
-        debugShowCheckedModeBanner: false,
-      
       ),
     );
   }
